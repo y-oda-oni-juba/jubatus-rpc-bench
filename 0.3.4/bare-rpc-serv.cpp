6f76a87c118bf95a07094df45132e5388b0fb1c9
y_oda <oda@onicos.co.jp>
2013-02-08 16:38:13 +0900

    add pficommon-mprpc bare rpc-server

---
+++ 0.3.4/bare-rpc-serv.cpp
@@ -0,0 +1,187 @@
+// -*- mode: c++; coding: utf-8-unix -*-
+
+#include <getopt.h>
+#include <iostream>
+#include <sstream>
+
+#include <msgpack.hpp>
+#include <pficommon/lang/bind.h>
+#include <pficommon/network/mprpc/rpc_server.h>
+
+#include "exception.h"
+#include "utils.h"
+#include "nullalgo_types.hpp"
+
+static const char *version_string = "1.0.0.130207a";
+
+namespace jubatus {
+namespace rpc_bench {
+
+class BareServer {
+public:
+  BareServer(int port, int thread_num) :
+    port_(port), thread_num_(thread_num), instance_(100000) {
+
+    create_stock_datum();
+    register_methods();
+  }
+
+  void register_methods() {
+    using pfi::lang::bind;
+    using pfi::lang::_1;
+
+    instance_.add<jubatus::datum(std::string)>( "query_cht", 
+                                                bind( &BareServer::query_cht, 
+                                                      this, _1 ) );
+    instance_.add<jubatus::datum(std::string)>( "query_cht_nolock", 
+                                                bind( &BareServer::query_cht_nolock,
+                                                      this, _1 ) );
+  }
+
+  void start() {
+    instance_.create( port_ );
+    instance_.run( thread_num_, /*sync*/ false );
+
+    std::cerr << "bare-pc-serv: started: "
+              << "port=" << port_ << ", #thread=" << thread_num_ << std::endl;
+  }
+
+  void join() {
+    instance_.join();
+  }
+
+  void stop() {
+    instance_.stop();
+  }
+
+private:
+  jubatus::datum query_cht(const std::string &param) {
+    return get_stock_datum( param );
+  }
+  jubatus::datum query_cht_nolock(const std::string &param) {
+    return get_stock_datum( param );
+  }
+
+  void create_stock_datum() {
+    using namespace std;
+
+    // string datum
+    string_datum_.string_values.push_back( make_pair( string("s"), 
+                                                      string( string_size_, 's')));
+
+    // large string datum
+    large_string_datum_.string_values.push_back( make_pair( string("l"),
+                                                            string( large_string_size_, 'l')));
+
+    // number datum
+    for(int i = 0; i < number_num_; ++i ) {
+      std::stringstream key_ss;
+      key_ss << i;
+      number_datum_.num_values.push_back( make_pair( key_ss.str(), double(i)));
+    }
+  }
+
+  jubatus::datum get_stock_datum( const std::string &result_type ) {
+    if ( result_type == "large" )
+      return large_string_datum_;
+    else if ( result_type == "number" )
+      return number_datum_;
+    else
+      return string_datum_;
+  }
+  
+  int port_;
+  int thread_num_;
+
+  pfi::network::mprpc::rpc_server instance_;
+
+  jubatus::datum string_datum_;
+  jubatus::datum large_string_datum_;
+  jubatus::datum number_datum_;
+
+  static const int string_size_ = 32;
+  static const int large_string_size_ = 1024 * 1024;
+  static const int number_num_ = 1;
+};
+
+} // rpc_bench
+} // jubatus
+
+//// main
+
+static void show_version() {
+  std::cout << "bare-rpc-serv version " << version_string << std::endl;
+}
+
+static void show_usage( std::ostream &out = std::cerr ) {
+  out << "Usage: bare-rpc-serv [Options]" << std::endl
+      << "Options:" << std::endl
+      << "  --port PORT" << std::endl
+      << "  --thread #THREAD" << std::endl
+      << "  --version" << std::endl
+      << "  --help" << std::endl;
+}
+
+int main(int argc, char **argv) {
+  int port = 9000;
+  int thread_num = 2;
+  
+  enum {
+    OPTION_PORT,
+    OPTION_THREAD,
+
+    OPTION_VERSION,
+    OPTION_HELP,
+  };
+  struct option longopts[] = {
+    { "port",		required_argument, NULL, OPTION_PORT },
+    { "thread",         required_argument, NULL, OPTION_THREAD },
+
+    { "version",	no_argument,       NULL, OPTION_VERSION },
+    { "help",           no_argument,       NULL, OPTION_HELP },
+
+    { 0, 0, 0, 0 }
+  };
+
+  try {
+    using jubatus::rpc_bench::parse_positive_number;
+
+    int opt = 0;
+    while( (opt = getopt_long_only( argc, argv, "", longopts, NULL )) != -1 ) {
+      switch(opt) {
+      case OPTION_PORT:
+        port = parse_positive_number( "port", optarg );
+        break;
+      case OPTION_THREAD:
+        thread_num = parse_positive_number( "thread", optarg );
+        break;
+
+      case OPTION_VERSION:
+        show_version();
+        return 0;
+      case OPTION_HELP:
+        show_usage( std::cout );
+        return 0;
+      
+      case '?':
+        show_usage( std::cerr);
+        return 1;
+      }
+    }
+    
+  } catch( const jubatus::rpc_bench::InvalidOption &e ) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
+  }
+
+  try {
+    jubatus::rpc_bench::BareServer server( port, thread_num );
+    server.start();
+    server.join();
+
+  } catch( std::exception &e ) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 2;
+  }
+  return 0;
+}
